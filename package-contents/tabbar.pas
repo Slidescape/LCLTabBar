3fae195ad7af270c957cfa396512268851221cf2
J <142033890+Slidescape@users.noreply.github.com>
2025-10-13 10:23:28 +0100

    Added comments to TTabBar methods, plus minor tweaks.

--- package-contents/tabbar.pas
+++ package-contents/tabbar.pas
@@ -17,6 +17,8 @@ interface uses
 
 type
 
+  { TTabData - Data per tab. }
+
   TTabData = record
     Caption: String;
     Enabled: Boolean;
@@ -25,30 +27,31 @@ type
     end;
   TTabArray = array of TTabData;
 
-  { TTabBar }
+  { TTabBar - The TabBar control itself. }
 
   TTabBar = class(TCustomControl)
   private
     FTabs: TStrings;
     FTabCount: Integer;
-    FTabWidth: Double;
-    FTabData: TTabArray;
     FTabIndex: Integer;
+    FTabWidth: Double;
+    FTabMinWidth: Integer;
+    FTabCurve: Integer;
+    FTabData: TTabArray;
     FOnSelect: TNotifyEvent;
     procedure AttachObserver;
-    //function CalculateTextCoords(ACaption: String): TPoint;
     function RoundToNearest(Value: Double): Integer;
-    procedure SelectALowerTab;
-    procedure SetTabEnabled(AValue: Boolean); overload;
-    function GetTabEnabled: Boolean;
-    function ShortenCaptionToFit(ARect: TRect; ACaption: String): String;
-    function TabIsEnabled(Index: Integer): Boolean;
     procedure PaintBackground;
     procedure PaintCaption(ACaption: String; x: Integer;
       TabEnabled: Boolean=True);
     procedure PaintHighlight(x: Integer);
     procedure PaintSeparator(AIndex, x: Integer);
     procedure PaintTabs;
+    procedure SelectALowerTab;
+    procedure SetTabEnabled(AValue: Boolean); overload;
+    function GetTabEnabled: Boolean;
+    function ShortenCaptionToFit(ARect: TRect; ACaption: String): String;
+    function TabIsEnabled(Index: Integer): Boolean;
   protected
     procedure Paint; override;
     procedure TextChangeObserved; virtual;
@@ -61,10 +64,10 @@ type
     destructor Destroy; override;
     procedure SetTabEnabled(Index: Integer; AValue: Boolean); overload;
     property TabCount: Integer read FTabCount;
+    property TabEnabled: Boolean read GetTabEnabled write SetTabEnabled;
   published
     property Tabs: TStrings read FTabs write SetTabCaptions;
     property TabIndex: Integer read FTabIndex write SetTabIndex;
-    property TabEnabled: Boolean read GetTabEnabled write SetTabEnabled;
     property Align;
     property Anchors;
     property BiDiMode;
@@ -91,6 +94,8 @@ type
     property OnMouseUp;
     property OnResize;
   end;
+
+  { TTsObserver - TStrings observer to detect any changes to tab captions. }
 
   TTsObserver = class(TObject,IFPObserver)
   public
@@ -146,6 +151,8 @@ begin
   Width:=250;
   Height:=25;
   FTabWidth:=60;
+  FTabMinWidth:=32;
+  FTabCurve:=5;
   FTabs:=TTextStrings.Create;
   AttachObserver;
   //if FTabs.CommaText='' then FTabs.CommaText:='One,Two,Three';
@@ -158,6 +165,7 @@ begin
   //Do I need to free the observer here?
   end;
 
+{ Create and attach an observer to report any changes to caption strings. }
 procedure TTabBar.AttachObserver;
 var
   subject: TStrings;
@@ -169,15 +177,23 @@ begin
   if Supports(subject, IFPObserved, interf) then
   begin
     interf.FPOAttachObserver(observer); //Attach observer to subject interface.
-    observer.TabBar:=Self;
+    observer.TabBar:=Self;              //Observer will need to notify TabBar.
     end;
+  end;
+
+{ Methods to paint the TabBar & Tabs... }
+
+procedure TTabBar.Paint;
+begin
+  PaintBackground;
+  PaintTabs;
   end;
 
 procedure TTabBar.PaintBackground;
 begin
   Canvas.Brush.Color:=FBarBackground;
   Canvas.Pen.Color:=FBarBorder;
-  Canvas.RoundRect(ClientRect,9,9);
+  Canvas.RoundRect(ClientRect,FTabCurve,FTabCurve);
   end;
 
 procedure TTabBar.PaintTabs;
@@ -188,7 +204,7 @@ var
   bTabEnabled: Boolean;
 begin
   if TabCount<1 then Exit;
-  FTabWidth:=Max((Width/TabCount),32);
+  FTabWidth:=Max((Width/TabCount),FTabMinWidth);
   x:=0;
   for f:=0 to TabCount-1 do begin
     iX:=RoundToNearest(x);
@@ -202,32 +218,7 @@ begin
     end;
   end;
 
-{function TTabBar.CalculateTextCoords(ACaption: String): TPoint;
-var
-  tx: TSize;
-begin
-  tx:=Canvas.TextExtent(ACaption);
-  Result.X:=(Trunc(FTabWidth)-tx.Width) div 2;
-  Result.Y:=(Height-tx.Height) div 2;
-  end;}
-
-function TTabBar.RoundToNearest(Value: Double): Integer;
-begin
-  if Value.Frac<5 then Result:=Trunc(Value) else Result:=Trunc(Value)+1;
-  end;
-
-function TTabBar.ShortenCaptionToFit(ARect: TRect; ACaption: String): String;
-var
-  iFitCount: Integer;
-begin
-  if UTF8Length(ACaption)>1 then begin
-    iFitCount:=Canvas.TextFitInfo(ACaption,ARect.Width);
-    if iFitCount<UTF8Length(ACaption)
-      then ACaption:=ACaption.Remove(iFitCount-2)+'…';
-    end;
-  Result:=ACaption;
-  end;
-
+{ For the currently selected tab, we paint a highlight rectangle. }
 procedure TTabBar.PaintHighlight(x: Integer);
 var
   iRight: Integer;
@@ -236,9 +227,16 @@ begin
   Canvas.Pen.Color:=FBarBorder;
   iRight:=x+RoundToNearest(FTabWidth)-1;
   if iRight>Width then iRight:=Width;
-  Canvas.RoundRect(Rect(x,0,iRight,Height),5,5);
+  Canvas.RoundRect(Rect(x,0,iRight,Height),FTabCurve,FTabCurve);
   end;
 
+{ Round to nearest integer - we don't want bankers rounding. }
+function TTabBar.RoundToNearest(Value: Double): Integer;
+begin
+  if Value.Frac<5 then Result:=Trunc(Value) else Result:=Trunc(Value)+1;
+  end;
+
+{ Paint a separator between unselected tabs only. }
 procedure TTabBar.PaintSeparator(AIndex,x: Integer);
 var
   sSeparator: String;
@@ -252,6 +250,8 @@ begin
   Canvas.TextOut(x-1,y-1,sSeparator);
   end;
 
+{ Paint the caption inside a tab rectangle, taking into account whether the tab
+  and/or control is enabled. }
 procedure TTabBar.PaintCaption(ACaption: String; x: Integer;
   TabEnabled: Boolean=True);
 var
@@ -272,26 +272,22 @@ begin
   Canvas.TextRect(rtTab,0,0,ACaption,ttStyle);
   end;
 
-procedure TTabBar.Paint;
-begin
-  PaintBackground;
-  PaintTabs;
-  end;
-
-procedure TTabBar.SelectALowerTab;
+{ If a caption is wider than the space available on the tab, we shorten it,
+  and append an ellipsis. }
+function TTabBar.ShortenCaptionToFit(ARect: TRect; ACaption: String): String;
 var
-  f,i: Integer;
+  iFitCount: Integer;
 begin
-  i:=-1;
-  for f:=FTabCount-1 downto 0 do begin
-    if TabIsEnabled(f) then begin
-      i:=f;
-      Break;
-      end;
+  if UTF8Length(ACaption)>1 then begin
+    iFitCount:=Canvas.TextFitInfo(ACaption,ARect.Width);
+    if iFitCount<UTF8Length(ACaption)
+      then ACaption:=ACaption.Remove(iFitCount-2)+'…';
     end;
-  SetTabIndex(i);
+  Result:=ACaption;
   end;
 
+{ The observer attached to FTabs has reported a change in the caption strings,
+  so we update our tab data and repaint. }
 procedure TTabBar.TextChangeObserved;
 var
   OldCount,f: Integer;
@@ -307,6 +303,24 @@ begin
   Invalidate;
   end;
 
+{ If the currently selected tab index no longer exists i.e. tab count has been
+  reduced - we select a lower tab index, by picking the highest available
+  (enabled) tab, or -1 if there are no enabled tabs. }
+procedure TTabBar.SelectALowerTab;
+var
+  f,i: Integer;
+begin
+  i:=-1;
+  for f:=FTabCount-1 downto 0 do begin
+    if TabIsEnabled(f) then begin
+      i:=f;
+      Break;
+      end;
+    end;
+  SetTabIndex(i);
+  end;
+
+{ Assign new tab captions - called by the IDE Form Designer Strings editor. }
 procedure TTabBar.SetTabCaptions(const AValue: TStrings);
 begin
   if (AValue <> nil) then begin
@@ -315,6 +329,7 @@ begin
     end;
   end;
 
+{ Returns True if the specified tab is Enabled. }
 function TTabBar.TabIsEnabled(Index: Integer): Boolean;
 begin
   if {(Index<0) or} (Index>Length(FTabData)-1) then
@@ -323,16 +338,19 @@ begin
   if Index<0 then Result:=False else Result:=FTabData[Index].Enabled;
   end;
 
+{ Returns True if the current tab is Enabled. }
 function TTabBar.GetTabEnabled: Boolean;
 begin
   Result:=TabIsEnabled(TabIndex);
   end;
 
+{ Sets the Enabled state of the current tab. }
 procedure TTabBar.SetTabEnabled(AValue: Boolean);
 begin
   SetTabEnabled(TabIndex,AValue);
   end;
 
+{ Sets the Enabled state of the specified tab. }
 procedure TTabBar.SetTabEnabled(Index: Integer; AValue: Boolean);
 begin
   if (Index<0) or (TabIsEnabled(Index)=AValue) then Exit;
@@ -340,6 +358,10 @@ begin
   Invalidate;
   end;
 
+{ Select the specified tab, making it the current highlighted tab. If the
+  specified tab index is too high, the highest tab is selected. Specify -1 to
+  have no tab selected. Note that unlike clicking on a tab, this method can
+  be called to force select a tab that is not currently Enabled. }
 procedure TTabBar.SetTabIndex(Value: Integer);
 begin
   if FTabIndex<>Value then begin
@@ -351,6 +373,8 @@ begin
     end;
   end;
 
+{ The TabBar has been clicked. Determine which tab was clicked and select it.
+  Setting TabIndex to a new value Invalidates the control so it will repaint. }
 procedure TTabBar.MouseDown(Button: TMouseButton; Shift: TShiftState; X,
   Y: Integer);
 var
@@ -358,11 +382,20 @@ var
 begin
   inherited MouseDown(Button,Shift,X,Y);
   if TabCount>0 then begin
-    tw:=Max(Width div TabCount,32)+1;
+    tw:=Max(Width div TabCount,FTabMinWidth)+1;
     i:=X div tw;
     if (i>-1) and (i<TabCount) and (FTabData[i].Enabled) then TabIndex:=i;
     end;
   end;
+
+{function TTabBar.CalculateTextCoords(ACaption: String): TPoint;
+var
+  tx: TSize;
+begin
+  tx:=Canvas.TextExtent(ACaption);
+  Result.X:=(Trunc(FTabWidth)-tx.Width) div 2;
+  Result.Y:=(Height-tx.Height) div 2;
+  end;}
 
 procedure Register;
 begin
